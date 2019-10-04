bb511af112ab27d4ffdc8015f2ec64d41c9d8975
JJ Thissell <chief@JJs-MacBook-Pro.local>
2019-10-04 18:26:38 -0400

    first iteration of search activity.

--- app/src/main/java/com/sandpeople/starwarsml/MainActivity.java
+++ app/src/main/java/com/sandpeople/starwarsml/MainActivity.java
@@ -1,10 +1,14 @@
 package com.sandpeople.starwarsml;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    //holds the twitter username entered
+    private static String twitterHandle = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
