6c8c71b856288a653b73737923d511e40a8b9447
JJ Thissell <chief@JJs-MacBook-Pro.local>
2019-10-04 19:41:43 -0400

    adding edited files.

--- app/src/main/java/com/sandpeople/starwarsml/MainActivity.java
+++ app/src/main/java/com/sandpeople/starwarsml/MainActivity.java
@@ -4,15 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    //private TextInputEditText twitterHandle = findViewById(R.id.TwitterHandle);
+
+
     //holds the twitter username entered
-    private static String twitterHandle = "";
+    //private static String twitterHandle = "";
+
+    //create an instance of custom text watcher class
+    CustomWatcher watcher = new CustomWatcher();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+    Button launchButton = findViewById(R.id.launch_button);
+    launchButton.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            //System.out.println(twitterHandle.toString());
+        }
+
+    });
+
+
+}
 }
