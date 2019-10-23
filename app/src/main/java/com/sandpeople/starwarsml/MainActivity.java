9d3ba89a7b6da3cfeaaa9a27e314f06fd41e815d
ajsv <alexanderjswanson@icloud.com>
2019-10-23 16:32:14 -0400

    Update MainActivity.java
    Updated MainActivity to be able to transition to other activities. @chiefjota , look at the method 'enteredTwitterHandleIsValid' and put the validation there, or if you want to change it let me know

--- app/src/main/java/com/sandpeople/starwarsml/MainActivity.java
+++ app/src/main/java/com/sandpeople/starwarsml/MainActivity.java
@@ -1,15 +1,14 @@
 package com.sandpeople.starwarsml;
 
-import androidx.annotation.ColorInt;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -40,57 +39,74 @@ public class MainActivity extends AppCompatActivity {
         launchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //System.out.println(twitterHandle.toString());
+
+                Log.i("MainActivity::" + R.id.launch_button,"Currently skipping username verification.");
+                if (enteredTwitterHandleIsValid()) {
+
+                }
+
             }
 
         });
 
     }
-        private final TextWatcher handleWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    /**
+     * The method to verify if a user-entered Twitter handle is a valid, existing handle.
+     * Currently hard-coded for development.
+     * @return Whether or not the Twitter handle the user entered exists.
+     */
+    private boolean enteredTwitterHandleIsValid() {
+
+        return true;
+
+    }
+
+    private final TextWatcher handleWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            String username = s.toString();
+            if(username.length() == 0 && start == 0 && count == 0 && after == 0){
+                twitterHandle.setBackgroundColor(Color.YELLOW);
+            }
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String username = s.toString();
+            //no text has been entered
+            if(username.length() == 0){
+                twitterHandle.setBackgroundColor(Color.YELLOW);
+                twitterHandle.getText();
+            }
+            //the background will change to red if the
+            //number of characters is odd and not 0
+
+            else if(username.length() % 2 != 0) {
+                twitterHandle.setBackgroundColor(Color.RED);
+                twitterHandle.getText();
+
+            }
+            //the background will change to blue if the
+            //number of characters is even and not 0
+            else{
+                twitterHandle.setBackgroundColor(Color.BLUE);
+                twitterHandle.getText();
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
                 String username = s.toString();
-                if(username.length() == 0 && start == 0 && count == 0 && after == 0){
-                    twitterHandle.setBackgroundColor(Color.YELLOW);
-                }
-
+                //if the character count is 15 and there is
+            //no more input from the keyboard
+            if(username.length() == USERNAMELIMIT){
+                twitterHandle.getText();
+                System.out.println(username);
             }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String username = s.toString();
-                //no text has been entered
-                if(username.length() == 0){
-                    twitterHandle.setBackgroundColor(Color.YELLOW);
-                    twitterHandle.getText();
-                }
-                //the background will change to red if the
-                //number of characters is odd and not 0
-
-                else if(username.length() % 2 != 0) {
-                    twitterHandle.setBackgroundColor(Color.RED);
-                    twitterHandle.getText();
-
-                }
-                //the background will change to blue if the
-                //number of characters is even and not 0
-                else{
-                    twitterHandle.setBackgroundColor(Color.BLUE);
-                    twitterHandle.getText();
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                    String username = s.toString();
-                    //if the character count is 15 and there is
-                //no more input from the keyboard
-                if(username.length() == USERNAMELIMIT){
-                    twitterHandle.getText();
-                    System.out.println(username);
-                }
-
-            }
-        };
+        }
+    };
 }
 
