5d682abf20dfab084a74c52a4d0ff0d4a4751b03
ajsv <alexanderjswanson@icloud.com>
2019-11-29 16:53:15 -0500

    Cleaned up code for Lambda execution and implemented prediction forwarding

--- app/src/main/java/com/sandpeople/starwarsml/SearchActivity.java
+++ app/src/main/java/com/sandpeople/starwarsml/SearchActivity.java
@@ -1,32 +1,37 @@
 package com.sandpeople.starwarsml;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
-import java.lang.reflect.Type;
-import java.util.List;
 
 
 public class SearchActivity extends AppCompatActivity {
 
+    // TODO: HANDLE_INVALID
+    private enum LambdaResponseResult {
+        SUCCESS,
+        TWEETS_LAMBDA_FAILED,
+        ML_LAMBDA_FAILED
+    }
+
     protected static Boolean DEV_MODE = true;
-    private TextInputEditText twitterHandle;
-    private static String userName;
+    private TextInputEditText twitterHandleInput;
+    private static String twitterHandle;
+    private LambdaResponseResult lambdaResponseResult;
     private String predictedCharacter;
-    private JsonObject tweetsLambdaResponse = new JsonObject();
-    private String mlLambdaResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +39,10 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         /*Initializing views*/
-        twitterHandle = findViewById(R.id.TwitterHandle);
-        twitterHandle.addTextChangedListener(handleWatcher);
+        twitterHandleInput = findViewById(R.id.twitter_handle);
+        twitterHandleInput.addTextChangedListener(handleWatcher);
+
+        if (DEV_MODE) twitterHandle = "elonmusk";
 
         setThreadPolicy();
 
@@ -44,8 +51,8 @@ public class SearchActivity extends AppCompatActivity {
     private void setThreadPolicy() {
         int SDK_INT = android.os.Build.VERSION.SDK_INT;
         if (SDK_INT > 8) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                    .permitAll().build();
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll()
+                    .build();
             StrictMode.setThreadPolicy(policy);
             //your codes here
 
@@ -63,23 +70,43 @@ public class SearchActivity extends AppCompatActivity {
      *
      * @param view Context current view.
      */
-    public void goToTransitionActivity(View view) {
+    public void submitHandle(View view) {
 
-        if (DEV_MODE) userName = "elonmusk";
+        // Get entered twitter handle.
+        Editable editable = twitterHandleInput.getText();
+        twitterHandle = editable == null ? "" : editable.toString();
 
+        // Execute the Lambda.
         new Lambda(this).execute();
-//        new MlLambda(this, tweetsLambdaResponse).execute();
 
+        // Begin Lambda-processing animation.
 
-//        if (lambdaResponse.get("error") != null) {
-//            Snackbar.make(view, "Enter a valid Twitter handle, you must", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show();
-//        } else {
-//            if(DEV_MODE) System.out.println("GOING TO TRANSITION_ACTIVITY FROM MAIN_ACTIVITY");
-//            Intent intent = new Intent(this, TransitionActivity.class);
-//            startActivity(intent);
-//        }
+    }
 
+    /**
+     * Determines the correct action based on the result of the Lambda thread's execution.
+     * @param result
+     */
+    private void processLambdaResult(LambdaResponseResult result) {
+        switch (result) {
+            case SUCCESS:
+                // Move on to the next activity.
+                goToTransitionActivity();
+                break;
+            case TWEETS_LAMBDA_FAILED:
+                // Indicate that the entered Twitter handle was not valid.
+                Toast.makeText(getApplicationContext(), "Enter a valid Twitter handle, you must...", Toast.LENGTH_LONG).show();
+                break;
+            case ML_LAMBDA_FAILED:
+                break;
+        }
+    }
+
+    private void goToTransitionActivity() {
+        if(DEV_MODE) System.out.println("GOING TO TRANSITION_ACTIVITY FROM MAIN_ACTIVITY");
+        Intent intent = new Intent(this, TransitionActivity.class);
+        intent.putExtra("predictedCharacter", predictedCharacter);
+        startActivity(intent);
     }
 
     private final TextWatcher handleWatcher = new TextWatcher() {
@@ -95,13 +122,14 @@ public class SearchActivity extends AppCompatActivity {
 
         @Override
         public void afterTextChanged(Editable s) {
-            userName = s.toString();
+            twitterHandle = s.toString();
         }
     };
 
     // TODO: Correctly implement error handling to present errors cleanly.
     private static class Lambda extends AsyncTask<Void, Void, JsonObject> {
 
+        // Reference to SearchActivity.
         private WeakReference<SearchActivity> activityReference;
 
         Lambda(SearchActivity context) {
@@ -110,33 +138,36 @@ public class SearchActivity extends AppCompatActivity {
 
         @Override
         protected JsonObject doInBackground(Void... params) {
-
-            JsonObject lambdaResponse = LambdaClient.execute(userName);
-
-            if (DEV_MODE) {
-                Type type = new TypeToken<List<String>>(){}.getType();
-                String data = lambdaResponse.get("tweets").toString();
-                List<String> tweets = LambdaClient.gson.fromJson(data, type);
-                System.out.println("Received tweets: " + tweets);
-            }
-
-            return lambdaResponse;
-
+            return LambdaClient.execute(twitterHandle);
         }
 
-        // MAYBE CREATE A BOOLEAN???????
         @Override
         protected void onPostExecute(JsonObject result) {
-            activityReference.get().tweetsLambdaResponse = result;
-            if (result != null) {
+
+            // Define reference to SearchActivity
+            SearchActivity activity = activityReference.get();
+
+            if (result.get("error") != null) {
+                activity.lambdaResponseResult = LambdaResponseResult.TWEETS_LAMBDA_FAILED;
+            } else {
                 try {
+                    // TODO: Improve code and error handling. Should maybe return an object that contains request response result and data.
+                    // Clean 'result', execute MLAPI, and define the predicted character
                     result.remove("prof_image"); result.remove("user_exists");
-                    String prediction = LambdaClient.predictCharacter(result);
-                    if (DEV_MODE) System.out.println("Predicted: " + prediction);
+                    String mlLambdaResponse = LambdaClient.predictCharacter(result);
+                    activity.lambdaResponseResult = LambdaResponseResult.SUCCESS;
+                    activity.predictedCharacter = mlLambdaResponse;
+                    if (DEV_MODE) System.out.println("Predicted: " + mlLambdaResponse + "for " + twitterHandle);
                 } catch (IOException e) {
+                    activity.lambdaResponseResult = LambdaResponseResult.ML_LAMBDA_FAILED;
                     e.printStackTrace();
                 }
             }
+
+            // Callback to process the Lambda execution's result
+            System.out.println(activity.lambdaResponseResult);
+            activity.processLambdaResult(activity.lambdaResponseResult);
+
         }
 
     }
